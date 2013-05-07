e7b7ae3957daad6b7cf233fca97fa926d51d79b5
Todd Taomae <ttaomae@gmail.com>
2013-05-06 17:14:48 -1000

    add webjars and Twitter Bootstrap dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -4,18 +4,21 @@ import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "book_exchange"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appName = "book_exchange"
+  val appVersion = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
     javaCore,
     javaJdbc,
-    javaEbean
+    javaEbean,
+    "org.webjars" % "webjars-play" % "2.1.0",
+    "org.webjars" % "bootstrap" % "2.3.1"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    // Add your own project settings here      
+    // Add your own project settings here
+    //resolvers += "webjars" at "http://webjars.github.com/m2"
   )
 
 }
